9f82e4b1325c86cba5d07a84f03371cfdefdc723
Midnight Mirage Softworks <midnightmiragesoftworks@gmail.com>
2025-03-09 17:26:46 +0000

    Tests and cute_tiled.h integration

--- app/src/main/cpp/main.c
+++ app/src/main/cpp/main.c
@@ -19,11 +19,8 @@
 
 #include "raymob.h" // This header can replace 'raylib.h' and includes additional functions related to Android.
 #include <math.h>
-
-//------------------------------------------------------------------------------------------
-// Types and Structures Definition
-//------------------------------------------------------------------------------------------
-typedef enum GameScreen { LOGO = 0, TITLE, GAMEPLAY, ENDING } GameScreen;
+#define CUTE_TILED_IMPLEMENTATION
+#include "cute_tiled.h"
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -32,71 +29,27 @@ int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 0;
+    const int screenHeight = 0;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic screen manager");
+    InitWindow(screenWidth, screenHeight, "SushiEngine");
 
-    GameScreen currentScreen = LOGO;
+    // NOTE: Textures MUST be loaded after Window initialization (OpenGL context is required)
+    int dataSize = 0;
+    unsigned char* mapData = LoadFileData("test.tmj", &dataSize);
 
-    // TODO: Initialize all required variables and load all required data here!
+    cute_tiled_map_t* map = cute_tiled_load_map_from_memory(mapData, dataSize, 0);
+    TraceLog(LOG_INFO, "%d  %d\n", map->width, map->height);
 
-    int framesCounter = 0;          // Useful to count frames
-
-    SetTargetFPS(60);               // Set desired framerate (frames-per-second)
-    //--------------------------------------------------------------------------------------
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //---------------------------------------------------------------------------------------
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         // Update
         //----------------------------------------------------------------------------------
-        switch (currentScreen)
-        {
-            case LOGO:
-            {
-                // TODO: Update LOGO screen variables here!
-
-                framesCounter++;    // Count frames
-
-                // Wait for 2 seconds (120 frames) before jumping to TITLE screen
-                if (framesCounter > 120)
-                {
-                    currentScreen = TITLE;
-                }
-            } break;
-            case TITLE:
-            {
-                // TODO: Update TITLE screen variables here!
-
-                // Press enter to change to GAMEPLAY screen
-                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
-                {
-                    currentScreen = GAMEPLAY;
-                }
-            } break;
-            case GAMEPLAY:
-            {
-                // TODO: Update GAMEPLAY screen variables here!
-
-                // Press enter to change to ENDING screen
-                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
-                {
-                    currentScreen = ENDING;
-                }
-            } break;
-            case ENDING:
-            {
-                // TODO: Update ENDING screen variables here!
-
-                // Press enter to return to TITLE screen
-                if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
-                {
-                    currentScreen = TITLE;
-                }
-            } break;
-            default: break;
-        }
+        // TODO: Update your variables here
         //----------------------------------------------------------------------------------
 
         // Draw
@@ -105,41 +58,9 @@ int main(void)
 
         ClearBackground(RAYWHITE);
 
-        switch(currentScreen)
-        {
-            case LOGO:
-            {
-                // TODO: Draw LOGO screen here!
-                DrawText("LOGO SCREEN", 20, 20, 40, LIGHTGRAY);
-                DrawText("WAIT for 2 SECONDS...", 290, 220, 20, GRAY);
+        //DrawTexture(texture, screenWidth/2 - texture.width/2, screenHeight/2 - texture.height/2, WHITE);
 
-            } break;
-            case TITLE:
-            {
-                // TODO: Draw TITLE screen here!
-                DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
-                DrawText("TITLE SCREEN", 20, 20, 40, DARKGREEN);
-                DrawText("PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN", 120, 220, 20, DARKGREEN);
-
-            } break;
-            case GAMEPLAY:
-            {
-                // TODO: Draw GAMEPLAY screen here!
-                DrawRectangle(0, 0, screenWidth, screenHeight, PURPLE);
-                DrawText("GAMEPLAY SCREEN", 20, 20, 40, MAROON);
-                DrawText("PRESS ENTER or TAP to JUMP to ENDING SCREEN", 130, 220, 20, MAROON);
-
-            } break;
-            case ENDING:
-            {
-                // TODO: Draw ENDING screen here!
-                DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
-                DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
-                DrawText("PRESS ENTER or TAP to RETURN to TITLE SCREEN", 120, 220, 20, DARKBLUE);
-
-            } break;
-            default: break;
-        }
+        DrawText("this IS a texture!", 360, 370, 10, GRAY);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
@@ -147,10 +68,10 @@ int main(void)
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
+    cute_tiled_free_map(map);
+    UnloadFileData(mapData);
 
-    // TODO: Unload all loaded data (textures, fonts, audio) here!
-
-    CloseWindow();        // Close window and OpenGL context
+    CloseWindow();                // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
     return 0;
